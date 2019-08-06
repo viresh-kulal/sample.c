59ff7f7dbe990db5c1fe79cf2681a770f1f6cb24
viresh-kulal <vireshkulal241999@gmail.com>
2019-08-06 16:13:29 +0530

    djkdasds

---
+++ sample.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	a=12;
+	b=49;
+	c=67;
+}
+	i76yf8iytjutyi76yf8iytjutyi76yf8iytjutyi76yf8iytjutyi76yf8iytjutyi76yf8iytjutyi76yf8iytjutyi76yf8iytjutyprntf("%d",a);
+
